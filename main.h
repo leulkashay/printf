1a8c717a5d60b18a120eabecde77ce7599a42c1a
amh21-Ha <111130584+amh21-Ha@users.noreply.github.com>
2022-10-18 22:47:38 +0300

    Update main.h

--- main.h
+++ main.h
@@ -2,8 +2,6 @@
 
 #define _MAIN_H_
 
-
-
 #include <stdarg.h>
 
 #include <stdio.h>
@@ -14,69 +12,34 @@
 
 #include <stdlib.h>
 
-
-
 #define OUTPUT_BUF_SIZE 1024
 
 #define BUF_FLUSH -1
 
-
-
 #define FIELD_BUF_SIZE 50
-
-
 
 #define NULL_STRING "(null)"
 
-
-
 #define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-
-
 
 #define CONVERT_LOWERCASE	1
 
 #define CONVERT_UNSIGNED	2
 
-
-
 /**
  *
- *  * struct parameters - parameters struct
- *
- *   *
- *
- *    * @unsign: flag if unsigned value
- *
- *     *
- *
- *      * @plus_flag: on if plus_flag specified
- *
- *       * @space_flag: on if hashtag_flag specified
- *
- *        * @hashtag_flag: on if _flag specified
- *
- *         * @zero_flag: on if _flag specified
- *
- *          * @minus_flag: on if _flag specified
- *
- *           *
- *
- *            * @width: field width specified
- *
- *             * @precision: field precision specified
- *
- *              *
- *
- *               * @h_modifier: on if h_modifier is specified
- *
- *                * @l_modifier: on if l_modifier is specified
- *
- *                 *
- *
- *                  */
-
-
+ * struct parameters - parameters structur
+ * @unsign: flag if unsigned value
+ * @plus_flag: on if plus_flag specified
+ * @space_flag: on if hashtag_flag specified
+ * @hashtag_flag: on if _flag specified
+ * @zero_flag: on if _flag specified
+ * @minus_flag: on if _flag specified
+ * @width: field width specified
+ * @precision: field precision specified
+ * @h_modifier: on if h_modifier is specified
+ * @l_modifier: on if l_modifier is specified
+ */
 
 typedef struct parameters
 
